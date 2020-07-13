87a5cc15edafebbaa5d9be1987f5116c4a6417be
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-07-13 16:55:19 +0530

    write a test for Card and CardList

---
+++ src/components/Card.test.js
@@ -0,0 +1,8 @@
+import {shallow , mount, render} from 'enzyme';
+import React from 'react';
+import Card from './card';
+
+it('expect to render Card component', () =>{
+    expect(shallow(<Card/>)).toMatchSnapshot();
+})
+
